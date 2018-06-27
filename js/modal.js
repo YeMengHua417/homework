47daf54d9571dbaf3b68fec7bd3f565daabcb84f
hna <april1984417@163.com>
2018-06-27 15:04:09 +0800

    TW work modal

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
        this.opt = opt || {};
     };
 
+
     popModal.prototype.show = function () {
 
         var modalHTML="<div class='md-modal'>"+
@@ -24,8 +25,6 @@
         var modal =document.createElement("div");
         modal.innerHTML = modalHTML;
 
-
-
         parentNode.appendChild(modal);
 
         // Event.addEvent(document.querySelector(".md-modal"),'click',function (event) {
@@ -41,7 +40,47 @@
             parentNode.removeChild(modal);
         });
 
+
+        function getNextElement(element){  // ie6的一些bug
+            var e = element.nextSibling;
+            if(e == null){//测试同胞节点是否存在，否则返回空
+                return null;
+            }
+            if(e.nodeType==3){//如果同胞元素为文本节点
+                var two = getNextElement(e);
+                if(two.nodeType == 1)
+                    return two;
+            }else{
+                if(e.nodeType == 1){//确认节点为元素节点才返回
+                    return e;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+
         Event.addEvent(document.querySelector(".md-modal .btn-add-resource"),'click',function (event) {
+
+            var text = modal.querySelector(".resource-input").value;
+
+            if(text == ""){
+                return;
+            }
+
+            var resourceArr = text.split(",");
+
+            for(var index = 0;index < resourceArr.length;index++){
+                var resoureHTML = "<span>"+resourceArr[index]+"</span>"+
+                                  "<i class='icon icon-trash'></i>"
+                var resource = document.createElement("li");
+                resource.innerHTML = resoureHTML;
+                resource.setAttribute("class","browser-type");
+
+                getNextElement(modal.parentNode.parentNode).appendChild(resource)
+
+            }
+
             parentNode.removeChild(modal);
         });
 
